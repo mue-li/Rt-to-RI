758b7f28dd0d7684946777d2d45d8c021efea505
mue-li <lina.mueller@tu-dresden.de>
2025-07-04 11:18:44 +0200

    update

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Rt-to-RI
-Rt-to-RI is a Python tool that converts the retention time (Rt) into the corresponding retention index (RI). The resulting data set can be imported into a visualisation program and used to display GC chromatograms with the RI instead of the Rt on the abscissa.
+Rt-to-RI is a Python tool that converts the retention time (Rt) into the corresponding retention index (RI). The resulting data set can be imported into a visualisation programme and used to display GC chromatograms with the RI instead of the Rt on the abscissa.
 
 # Installation
 1) If necessary, install Python (programming language) and Anaconda (contains Python and other useful packages) as well as a text editor, for example Visula Studio Code.
--- app.py
+++ app.py
@@ -106,7 +106,7 @@ app.layout = dbc.Container(
                         html.Br(),
                         html.P(
                             [
-                                "Rt-to-RI is a Python tool that converts the retention time (Rt) into the corresponding retention index (RI). The resulting data set can be imported into a visualisation program and used to display GC chromatograms with the RI instead of the Rt on the abscissa.",
+                                "Rt-to-RI is a Python tool that converts the retention time (Rt) into the corresponding retention index (RI). The resulting data set can be imported into a visualisation programme and used to display GC chromatograms with the RI instead of the Rt on the abscissa.",
                                 html.Br(),
                                 html.Hr(),
                                 html.Strong("How do I use this application?"),
